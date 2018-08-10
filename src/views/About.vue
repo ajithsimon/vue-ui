3151faefc36f8edf2bc0b430e258ca8f2881a10c
ajithsimon <ajith@gympik.com>
2018-08-10 17:48:32 +0530

    ajax practise

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,19 +2,38 @@
   <div class="about">
     <h1>This is an about page</h1>
     <!-- <button v-on:click="getPost()">Get posts</button> -->
-    <div v-if="posts && posts.length" class="ui grid">
-      <div class="five column row">
-        <div class="column" v-for="post in posts">
-          <div class="ui card">
-            <div class="content">
-              <div class="header">{{post.title | truncate(40)}}</div>
-              <div class="meta">2 days ago</div>
-              <div class="description">
-                <p>{{post.body | truncate(100)}}</p>
+    <div class="ui page dimmer"></div>
+      <div v-if="posts && posts.length" class="ui grid">
+        <div class="five column row">
+          <div class="column" v-for="post in posts">
+            <div class="ui card">
+              <div class="content">
+                <div class="header">{{post.title | truncate(40)}}</div>
+                <div class="meta">2 days ago</div>
+                <div class="description">
+                  <p>{{post.body | truncate(100)}}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+    <div class="ui modal">
+      <i class="close icon"></i>
+      <div class="header">
+        Modal Title
+      </div>
+      <div class="image content">
+        <div class="image">
+          An image can appear on left or an icon
+        </div>
+        <div class="description">
+          A description can appear on the right
+        </div>
+      </div>
+      <div class="actions">
+        <div class="ui button">Cancel</div>
+        <div class="ui button">OK</div>
       </div>
     </div>
   </div>
@@ -31,17 +50,24 @@ export default{
   },
   created() {
     // getPost: function() {
-      this.$Progress.start()
-      axios.get(`http://jsonplaceholder.typicode.com/posts`)
-      .then(response => {
-        // JSON responses are automatically parsed.
-        this.$Progress.finish()
-        this.posts = response.data.slice(0,10)
-      })
-      .catch(e => {
-        this.errors.push(e)
-      })
+    this.$Progress.start();
+    $('.page.dimmer').dimmer('show');
+    axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.$Progress.finish();
+      $('.page.dimmer').dimmer('hide');
+      this.posts = response.data.slice(0,10)
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
     // }
+  },
+  mounted: function(){
+    $('.ui.modal')
+    .modal()
+    ;
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="home">
     <h2>Home Page</h2>
-    <div v-if="studioData" class="ui grid">
-      <div class="five column row">
-        <div class="column">
-          <div class="ui card">
-            <div class="content">
-              <div class="header">{{studioData.title }}</div>
-              <div class="description">
-                <p>{{studioData.description}}</p>
-                <p>Director: {{studioData.director}}, Producer: {{studioData.producer}}</p>
-                <p>Released at:{{studioData.released_date}}, IMDB: {{studioData.rt_score}}%</p>
-              </div>
-              <div class="description">
-                <a v-for="(person, key) in people" :href="person">{{key}}</a>
-              </div>
+    <div class="ui dimmer">
+      <div class="ui loader"></div>
+    </div>
+    <div v-if="studioData" class="ui grid container">
+      <div class="two column row">
+        <div class="ui card column">
+          <div class="content">
+            <div class="header">{{studioData.title }}</div>
+            <div class="description">
+              <p>{{studioData.description}}</p>
+              <p>Director: {{studioData.director}}, Producer: {{studioData.producer}}</p>
+              <p>Released at:{{studioData.released_date}}, IMDB: {{studioData.rt_score}}%</p>
+            </div>
+            <div class="description">
+              <p v-for="artist in artists">  {{artist.name}}<img src="https://source.unsplash.com/collection/190727/1600x900" alt="img" width="200px"/></p>
             </div>
           </div>
         </div>
@@ -30,21 +31,34 @@ export default {
   data(){
     return{
       studioData:[],
-      people:[],
+      artists:[],
       errors:[]
     }
   },
   created() {
-    this.$Progress.start()
+    this.$Progress.start();
+    $('.ui.dimmer').dimmer('show');
     axios.get(`https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49`)
     .then(response => {
-      this.$Progress.finish()
+      $('.ui.dimmer').dimmer('hide');
       this.studioData = response.data;
       this.people = this.studioData.people;
-      // console.log(response.data);
+      for(var i=0;i<this.people.length;i++){
+        axios.get(this.people[i])
+        .then(response => {
+          this.$Progress.finish();
+          this.artists.push(response.data);
+        })
+      }
     })
     .catch(e => {
       this.errors.push(e)
+    })
+
+    //unsplash
+    axios.get('https://source.unsplash.com/random')
+    .then(response => {
+      console.log(response.data);
     })
   }
 }
